b9202440894374978973657c19dbbe993b2accae
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-28 16:27:42 +0000

    Added improved edge case handling for punctuations.

--- project.py
+++ project.py
@@ -11,15 +11,26 @@ suffix: str = "yay" if len(sys.argv) < 3 else suffix_list[int(sys.argv[2])]
 
 pig_word_list: list = []
 for word in word_list:
-    if word[0] in vowels:
-        pig_word_list.append(word + suffix)
+    is_vowel = True if word[0] in vowels else False
+
+    punctuation: str = ""
+    if word[-1] not in vowels and word[-1] not in consonants:
+        punctuation = word[-1]
+        word = word[:-1]
+
+    if is_vowel:
+        pig_word_list.append(word + suffix + punctuation)
     else:
         temp: str = ""
         for i in range(len(word)):
             if word[i] not in vowels:
                 temp += word[i]
             else:
-                pig_word_list.append(word[i:] + temp.lower() + "ay")
+                pig_word_list.append(word[i:] + temp.lower() + "ay" + punctuation)
                 break
 
-print(pig_word_list)
+for index in range(len(pig_word_list)):
+    if index == 0:
+        print(pig_word_list[0].capitalize(), end=" ")
+        continue
+    print(pig_word_list[index], end=" ")
