534588ddd2692a257c67705cb93ba737b08fa25c
Lethios <204111230+Lethios@users.noreply.github.com>
2025-09-28 16:00:26 +0000

    Created initial structure for code

--- project.py
+++ project.py
@@ -1,1 +1,25 @@
+import sys
+import string
 
+vowels: set = set("aAeEiIoOuU")
+consonants: set = set(string.ascii_letters) - vowels
+suffix_list = {0: "yay", 1: "way", 2: "hay"}
+
+text: str = sys.argv[1]
+word_list: list = text.split()
+suffix: str = "yay" if len(sys.argv) < 3 else suffix_list[int(sys.argv[2])]
+
+pig_word_list: list = []
+for word in word_list:
+    if word[0] in vowels:
+        pig_word_list.append(word + suffix)
+    else:
+        temp: str = ""
+        for i in range(len(word)):
+            if word[i] not in vowels:
+                temp += word[i]
+            else:
+                pig_word_list.append(word[i:] + temp.lower() + "ay")
+                break
+
+print(pig_word_list)
